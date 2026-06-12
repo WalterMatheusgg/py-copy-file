9f3e822b59ed1d55efd39852c345f9e3b606a565
Walter_504 <157768009+WalterMatheusgg@users.noreply.github.com>
2026-06-12 15:44:52 -0300

    Enhance copy_file function with error handling

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 def copy_file(command: str) -> None:
-    _, old_file, new_file = command.split()
+    parts = command.split()
+
+    if len(parts) != 3:
+        return
+
+    _, old_file, new_file = parts
 
     if old_file == new_file:
         return
 
-    with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
-        file_out.write(file_in.read())
+    try:
+        with open(old_file, "r") as file_in, open(new_file, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        return
